b25331cc240e4ff5316bb929fda3bd0a3bd8896e
Maciej Kluczynski <maciekk97wp@wp.pl>
2022-05-29 14:49:12 +0200

    adjusting setExact for Android >= 11

--- app/src/main/java/com/kluczynski/maciej/intracompanycrowdsensingv3/domain/AlertManager.kt
+++ app/src/main/java/com/kluczynski/maciej/intracompanycrowdsensingv3/domain/AlertManager.kt
@@ -1,14 +1,18 @@
 package com.kluczynski.maciej.intracompanycrowdsensingv3.domain
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.provider.Settings
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat.startActivity
 import com.kluczynski.maciej.intracompanycrowdsensingv3.ReminderBroadcast
 import java.util.*
 
@@ -42,9 +46,20 @@ class AlertManager(var context: Context, var dateManager: DateManager) {
             }
         }
         val pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_IMMUTABLE)
+
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.set(AlarmManager.RTC_WAKEUP, sensingRequestAskTime.time, pendingIntent)
+        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.R){
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, sensingRequestAskTime.time, pendingIntent)
+        }else{
+            val canScheduleAlarm = alarmManager.canScheduleExactAlarms()
+            if (!canScheduleAlarm){
+                alarmManager.set(AlarmManager.RTC_WAKEUP, sensingRequestAskTime.time, pendingIntent)
+            }else{
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, sensingRequestAskTime.time, pendingIntent)
+            }
+        }
     }
+
 
     @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SimpleDateFormat")
